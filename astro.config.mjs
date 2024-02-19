76642d149df4d8d13fa389fba9e5b8a3ce948ef2
VV01T3K <wojteks.access@gmail.com>
2024-02-19 23:23:46 +0000

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,13 @@ import vercel from "@astrojs/vercel/serverless";
 import solidJs from "@astrojs/solid-js";
 import icon from "astro-icon";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs(), solidJs(), icon(), react(), tailwind()],
+  integrations: [sentry(), spotlightjs(), solidJs(), icon(), react(), tailwind(), mdx()],
   output: "hybrid",
   adapter: vercel()
 });
--- src/pages/math.json.js
+++ src/pages/math.json.js
@@ -1,7 +1,11 @@
 export const prerender = false;
 
+import { Database } from "bun:sqlite";
+
 export async function GET() {
     let number = Math.random();
+    // const data = `It was the best of times, it was the worst of times.`;
+    // await Bun.write("output.txt", data);
     return new Response(
         JSON.stringify({
             number,
