6e1af72c471bd3b19cb106d9c6702b92eb7f9699
VV01T3K <wojteks.access@gmail.com>
2024-02-19 22:48:33 +0000

    more integrateions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import spotlightjs from "@spotlightjs/astro";
 import vercel from "@astrojs/vercel/serverless";
 import solidJs from "@astrojs/solid-js";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs(), solidJs(), icon(), react()],
+  integrations: [sentry(), spotlightjs(), solidJs(), icon(), react(), tailwind()],
   output: "hybrid",
   adapter: vercel()
 });
