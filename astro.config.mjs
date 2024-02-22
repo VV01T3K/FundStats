51b670ca817ba71add185dafbccb1b3250d5fadd
VV01T3K <wojteks.access@gmail.com>
2024-02-22 19:31:31 +0000

    working solid-ui with astro localy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), tailwind({
+    applyBaseStyles: false
+  }), icon(), mdx()]
+});
