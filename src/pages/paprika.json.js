12173cd29ee7c62bf9e795565abc6a47fd6417b6
VV01T3K <wojteks.access@gmail.com>
2024-02-22 19:06:51 +0000

    clean slate

--- src/pages/paprika.json.js
+++
@@ -1,6 +0,0 @@
-export const prerender = false;
-
-export async function GET() {
-    const data = await fetch("https://api.coinmarketcap.com/aggr/v3/web/homepage");
-    return new Response(data.body);
-}
